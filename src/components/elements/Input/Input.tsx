fba04ba16e624fe4ac0399c463742963ed7b862a
Leonardo Caldeira <l.bassini@pitagorinesgroup.com>
2023-04-13 09:40:18 -0300

    fix(eslint): fixed eslint and prettier quotes

--- src/components/elements/Input/Input.tsx
+++ src/components/elements/Input/Input.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./Input.scss";
-import { InputProps } from "./types";
+import React from 'react';
+import './Input.scss';
+import { InputProps } from './types';
 
 const Input = (props: InputProps) => {
     const { label, value, onChange, error, errormessage, className, disabled } = props;
 
     return (
         <div
-            className={`input-wrapper ${error ? "onError" : ""} ${className ? className : ""}`}
+            className={`input-wrapper ${error ? 'onError' : ''} ${className ? className : ''}`}
             data-testid="input-wrapper"
         >
             <label>{label}</label>
--- src/tests/Input.test.tsx
+++ src/tests/Input.test.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
-import Input from "../components/elements/Input/Input";
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import Input from '../components/elements/Input/Input';
 
-describe("App", () => {
-    test("Should be able to render the Input on the screen", () => {
+describe('App', () => {
+    test('Should be able to render the Input on the screen', () => {
         const { getByText } = render(<Input label="test label" />);
 
-        expect(getByText("test label")).toBeInTheDocument();
+        expect(getByText('test label')).toBeInTheDocument();
     });
 
-    test("Should be able to render the Input on the screen with an error", () => {
+    test('Should be able to render the Input on the screen with an error', () => {
         const { container } = render(<Input label="test label" error />);
 
-        const errorExist = container.innerHTML.includes("onError");
+        const errorExist = container.innerHTML.includes('onError');
 
         expect(errorExist).toBe(true);
     });
 
-    test("Should be able to render the Input on the screen with an error message", () => {
+    test('Should be able to render the Input on the screen with an error message', () => {
         const { getByTestId } = render(<Input label="test label" errormessage="Mensagem de error" error />);
 
-        expect(getByTestId("error-message")).toBeInTheDocument();
+        expect(getByTestId('error-message')).toBeInTheDocument();
     });
 
-    test("Should be able to render the input disabled", () => {
+    test('Should be able to render the input disabled', () => {
         const { getByTestId } = render(<Input label="test label" disabled />);
 
-        expect(getByTestId("input")).toBeDisabled();
+        expect(getByTestId('input')).toBeDisabled();
     });
 
-    test("Should be able to render the input disabled", () => {
+    test('Should be able to render the input disabled', () => {
         const { getByTestId } = render(<Input label="test label" value="teste valor" />);
 
-        expect(getByTestId("input")).toHaveValue();
+        expect(getByTestId('input')).toHaveValue();
     });
 });
